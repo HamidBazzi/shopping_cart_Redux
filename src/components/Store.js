15e382565f7e9ddcbf570a603bd4607aa4f8ee35
HamidBazzi <bazzi.hamid@gmail.com>
2023-11-15 10:27:20 +0330

    shopping Cart with REDUX

---
+++ src/components/Store.js
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+// Components
+import Product from './shared/Product';
+
+// Style
+import styles from "./Store.module.css";
+import Loader from './Loader';
+
+import { fetchProducts } from '../redux/products/productsAction';
+
+const Store = () => {
+
+    const dispatch = useDispatch()
+    const productsState = useSelector(state => state.productsState)
+
+    useEffect(() => {
+        if(!productsState.products.length) dispatch(fetchProducts())
+    }, [])
+
+    return (
+        <div className={styles.container} >
+            {
+                productsState.loading ?
+                <Loader/> :
+                productsState.error ?
+                <p>Something went wrong</p> :
+                productsState.products.map(product => <Product key={product.id} productData={product}/>)
+            }
+        </div>
+    );
+};
+
+export default Store;
